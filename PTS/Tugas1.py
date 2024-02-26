71ccc1d0295e0bf630668da2993ec62a764af758
WildanNovanaFikri <140993618+WildanNovanaFikri@users.noreply.github.com>
2024-02-26 08:52:37 +0700

    Add files via upload

---
+++ PTS/Tugas1.py
@@ -0,0 +1,5 @@
+for x in range(1,101):
+    if(x%2 ==0):
+        print(x,"Bilangan Genap")
+    else:
+        print(x,"Bilangan Ganjil")
---
+++ PTS/Tugas2.py
@@ -0,0 +1,14 @@
+# Cek apakah sebuah bilangan prima
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Cetak semua bilangan prima dari 1 hingga 100
+print("Bilangan prima antara 1 dan 100:")
+for i in range(1, 100):
+    if is_prime(i):
+        print(i)
